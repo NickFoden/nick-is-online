81973cb5ff89c59837a6458b4abeb72f45219338
Nick Foden <nickfoden@gmail.com>
2018-01-28 18:56:44 -0500

    Added target so form keeps you on the page.

--- app.js
+++ app.js
@@ -1,18 +1,17 @@
-import axios from "axios";
 const API_BASE_URL = "https://proofing-server.herokuapp.com";
 
-export const sendMail = values => {
-  axios
-    .get(`${API_BASE_URL}/send`, {
-      headers: {
-        // Authorization: `Bearer ${authToken}`
-      },
-      body: JSON.stringify({
-        values
-      })
-    })
-    .then(result => {
-      console.log(result);
-    })
-    .catch(error => console.log(error));
-};
+// export const sendMail = values => {
+//   axios
+//     .get(`${API_BASE_URL}/send`, {
+//       headers: {
+//         // Authorization: `Bearer ${authToken}`
+//       },
+//       body: JSON.stringify({
+//         values
+//       })
+//     })
+//     .then(result => {
+//       console.log(result);
+//     })
+//     .catch(error => console.log(error));
+// };
