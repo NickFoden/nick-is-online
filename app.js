b220e0aa4fbe85021ab0895270653ba1de3c668e
Nick Foden <nickfoden@gmail.com>
2018-03-17 16:28:02 -0400

    Updated bio and added button option for longer bio

--- app.js
+++ app.js
@@ -2,3 +2,8 @@ $("#form-submit-button").on("click", function() {
   $(".contact-form").addClass("none");
   $("#thank-you").removeClass("none");
 });
+
+$("#expand-bio").on("click", function() {
+  $("#bio-long").removeClass("hide");
+  $("#expand-bio").addClass("hide");
+});
