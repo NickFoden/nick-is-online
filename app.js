56f6d8c5213ef1400c9dde12e50a52d1732bb171
Nick Foden <nickfoden@gmail.com>
2018-08-07 20:56:47 -0400

    Styling the icon links

--- app.js
+++ app.js
@@ -8,3 +8,27 @@ $("#expand-bio").on("click", function() {
   $("#bio-long").removeClass("hide");
   $("#expand-bio").addClass("hide");
 });
+
+var isInViewport = function(elem) {
+  var bounding = elem.getBoundingClientRect();
+  return (
+    bounding.bottom >= 25
+    // &&
+    // bounding.left >= 0 &&
+    // bounding.bottom <=
+    //   (window.innerHeight || document.documentElement.clientHeight) &&
+    // bounding.right <=
+    //   (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+var image = document.querySelector(".project-image");
+window.addEventListener(
+  "scroll",
+  function(event) {
+    if (isInViewport(image)) {
+      console.log("Working");
+      $("#project-image-id").removeClass("hide");
+    }
+  },
+  false
+);
